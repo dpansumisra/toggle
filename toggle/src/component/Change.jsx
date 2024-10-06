2255260c2c390ca4f1668fb5d8aca830a6a804ec
dpansumisra <dpansumisra@gmail.com>
2024-10-06 13:05:57 +0530

    Add

--- toggle/src/component/Change.jsx
+++ toggle/src/component/Change.jsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import  './mode.css'
 const Change = () => {
   const [color, setColor] = useState(true); 
+  const onClickformode = () =>{
+    if(color === true){
+        setColor(false);
+    }
+    else setColor(true);
+  }
   return (
     <div className={color ? 'light-mode' : 'dark-mode'}>
-      <button className="btn" onClick={() => setColor(!color)}>
+      <button className="btn" onClick={onClickformode}>
         Mode {color ? 'Light' : 'Dark'} 
       </button>
     </div>
