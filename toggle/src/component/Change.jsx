600ad3423e468f9e3a795446c6ead5a88683e7b2
dpansumisra <dpansumisra@gmail.com>
2024-10-06 12:42:47 +0530

    Add

--- toggle/src/component/Change.jsx
+++ toggle/src/component/Change.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import mode from './component/mode.css'
+import  './mode.css'
 const Change = () => {
   const [color, setColor] = useState(true); 
   return (
     <div className={color ? 'light-mode' : 'dark-mode'}>
       <button className="btn" onClick={() => setColor(!color)}>
-        Mode: {color ? 'Light' : 'Dark'} 
+        Mode {color ? 'Light' : 'Dark'} 
       </button>
     </div>
   );
